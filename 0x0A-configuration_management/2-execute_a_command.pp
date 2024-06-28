2387601d6d3ec0d775302216e9c4099c16002086
root <root@Joshua-YPC>
2024-06-28 23:17:55 +0000

    Identation fix on task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,8 @@
 # Using Puppet to create a manifest that kills a process named killmenow
 
 exec { 'kill_killmenow_process':
-  command     => 'pkill -f killmenow',
+  command      => 'pkill -f killmenow',
   #refreshonly => true,
   #onlyif      => 'pgrep -f killmenow',
-  path        => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
+  path         => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
 }
