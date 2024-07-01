e3ee3c846566a598d234e2c03af0f8ae5adc86c2
root <root@Joshua-YPC>
2024-07-01 04:52:55 +0000

    Few changes on identity path made on task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,30 +1,15 @@
 # Using Puppet to configure SSH Client
 
-file { '/etc/ssh/.ssh':
-  ensure => 'directory',
-  owner  => 'joshuayankson',
-  group  => 'joshuayankson',
-  mode   => '0700',
-}
-
-file { '/etc/ssh/.ssh/config':
-  ensure  => 'file',
-  owner   => 'joshuayankson',
-  group   => 'joshuayankson',
-  mode    => '0600',
-  require => File['/etc/ssh/.ssh'],
-}
-
 file_line { 'add_dentity_file':
+  require => 'present',
   path    => '/etc/ssh/.ssh/config',
   line    => 'IdentityFile ~/.ssh/school',
   match   => '^IdentityFile',
-  require => File['/etc/ssh/.ssh/config'],
 }
 
 file_line { 'passwd_auth_no':
+  require => 'present',
   path    => '/etc/ssh/.ssh/config',
   line    => 'PasswordAuthentication no',
   match   => '^PasswordAuthentication',
-  require => File['/etc/ssh/.ssh/config'],
 }
