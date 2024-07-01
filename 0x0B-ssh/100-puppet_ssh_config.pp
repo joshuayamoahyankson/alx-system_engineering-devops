1793fb823130f4343e85ca2a4aca21b2e97d8237
root <root@Joshua-YPC>
2024-07-01 04:10:40 +0000

    Using Puppet to connect to SSH Client config...

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -16,15 +16,15 @@ file { '/home/joshuayankson/.ssh/config':
 }
 
 file_line { 'add_dentity_file':
-  path  => '/home/joshuayankson/.ssh/config',
-  line  => 'IdentityFile ~./ssh/school',
-  match => '^IdentityFile',
+  path    => '/home/joshuayankson/.ssh/config',
+  line    => 'IdentityFile ~./ssh/school',
+  match   => '^IdentityFile',
   require => File['/home/joshuayankson/.ssh/config'],
 }
   
 file_line { 'passwd_auth_no':
-  path  => '/home/joshuayankson/.ssh/config',
-  line  => 'PasswordAuthentication no',
-  match => '^PasswordAuthentication',
+  path    => '/home/joshuayankson/.ssh/config',
+  line    => 'PasswordAuthentication no',
+  match   => '^PasswordAuthentication',
   require => File['/home/joshuayankson/.ssh/config'],
 }
